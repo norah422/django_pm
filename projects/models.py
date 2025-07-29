a970c84ce642f42cf146d7b05a205db9321e26c9
نورة الخريجة <neeemll90@gmail.com>
2025-07-29 10:11:13 +0300

    Add RTL & Arabic Support

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf.global_settings import AUTH_USER_MODEL
+from django.utils.translation import gettext as _
 
-# Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -11,10 +11,10 @@ class Category(models.Model):
 
 
 class Project_status(models.IntegerChoices):
-    PENDING = 1, 'pending'
-    COMPLETED = 2, 'completed'
-    POSTPONED = 3, 'postponed'
-    CANCELED = 4, 'canceled'
+    PENDING = 1, _('pending')
+    COMPLETED = 2, _('completed')
+    POSTPONED = 3, _('postponed')
+    CANCELED = 4, _('canceled')
 
 
 class Project(models.Model):
