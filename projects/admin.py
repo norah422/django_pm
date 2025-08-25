747531a4a27ac6031a9daa09efafa6ebec335ef8
نورة الخريجة <neeemll90@gmail.com>
2025-08-25 14:42:37 +0300

    Admin customization

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
-from projects.models import Category, Project, Task
+from django.db.models import Count
+from . import models
+from .models import Category
 
-admin.site.register(Category),
-admin.site.register(Project),
-admin.site.register(Task)
+admin.site.register(Category)
+
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display =['title', 'status', 'user', 'category', 'created_at', 'tasks_count']
+    list_per_page = 20
+    list_editable = ['status']
+    list_select_related = ['category', 'user']
+
+    def tasks_count(self, obj):
+        return obj.tasks_count
+
+    def get_queryset(self, request):
+        query = super().get_queryset(request)
+        query = query.annotate(tasks_count=Count('task'))
+        return query
+
+
+@admin.register(models.Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display =['id', 'description', 'project', 'is_completed']
+    list_editable = ['is_completed']
+    list_per_page = 20
--- projects/models.py
+++ projects/models.py
@@ -9,6 +9,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = _('Category')
+
 
 class Project_status(models.IntegerChoices):
     PENDING = 1, _('pending')
@@ -36,6 +40,10 @@ class Project(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = _('Project')
+        verbose_name_plural = _('Project')
+
 class Task(models.Model):
     description = models.TextField()
     is_completed = models.BooleanField(default=False)
@@ -43,3 +51,7 @@ class Task(models.Model):
 
     def __str__(self):
         return self.description
+
+    class Meta:
+        verbose_name = _('Task')
+        verbose_name_plural = _('Task')
